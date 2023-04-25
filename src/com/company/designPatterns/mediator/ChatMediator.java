b13d64ab6b801fdb1300a7819defd41076d56153
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-25 09:17:48 +0300

    ChatMediator

---
+++ src/com/company/designPatterns/mediator/ChatMediator.java
@@ -0,0 +1,22 @@
+package com.company.designPatterns.mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatMediator implements Mediator {
+    private List<Student> students = new ArrayList<>()
+
+    @Override
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public void sendMessage(Student sender, String message) {
+        for(Student student : students){
+            if (student != sender) {
+                student.receiveMessage(message);
+            }
+        }
+    }
+}
