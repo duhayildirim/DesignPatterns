eb669afd4c1fae361d5bfbea0d737bd233806077
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-25 15:09:38 +0300

    DisplayState

--- src/com/company/designPatterns/state/CurrentState.java
+++ src/com/company/designPatterns/state/CurrentState.java
@@ -1,31 +1,50 @@
 package com.company.designPatterns.state;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrentState {
-    private UserListState currentState;
+    private FilterState currentState;
     private List<User> userList;
 
     public CurrentState() {
         this.userList = new ArrayList<User>();
-        this.currentState = new UserListDisplayState(this);
+        this.currentState = new DisplayState(this);
     }
 
-    public void setCurrentState(UserListState state) {
+    public void setCurrentState(FilterState state) {
         this.currentState = state;
     }
 
     public void showAllUsers() {
-        // Tüm kullanıcıları listeleme işlemi
+        this.currentState.displayUserList();
     }
 
     public void filterByName(String name) {
-        // İsimle filtreleme işlemi
+        this.currentState.filterByName(name);
     }
 
     public void filterByAge(int age) {
-        // Yaşa göre filtreleme işlemi
+        this.currentState.filterByAge(age);
     }
 
-    public UserListState getUserListDisplayState() {
-        return new UserListDisplayState();
+    public FilterState getUserListDisplayState() {
+        return new DisplayState(this);
+    }
+
+    public FilterState getUserListFilteringByNameState(String name) {
+        return new FilteringByNameState(this, name);
+    }
+
+    public FilterState getUserListFilteringByAgeState(int age) {
+        return new FilteringByAgeState(this, age);
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void addUser(User user) {
+        userList.add(user);
     }
 }
