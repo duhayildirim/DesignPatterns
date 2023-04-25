3ca66741a93f9417ca0a1e78e76add723f1da6e4
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-25 09:18:58 +0300

    Student

--- src/com/company/designPatterns/mediator/ChatMediator.java
+++ src/com/company/designPatterns/mediator/ChatMediator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatMediator implements Mediator {
-    private List<Student> students = new ArrayList<>()
+    private List<Student> students = new ArrayList<>();
 
     @Override
     public void addStudent(Student student) {
@@ -13,7 +13,7 @@ public class ChatMediator implements Mediator {
 
     @Override
     public void sendMessage(Student sender, String message) {
-        for(Student student : students){
+        for (Student student : students) {
             if (student != sender) {
                 student.receiveMessage(message);
             }
