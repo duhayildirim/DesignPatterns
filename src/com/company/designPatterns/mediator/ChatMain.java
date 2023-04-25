526c2632c11eacd27884681ff998b1c88c6615de
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-25 09:37:56 +0300

    bug fix

--- src/com/company/designPatterns/mediator/ChatMain.java
+++ src/com/company/designPatterns/mediator/ChatMain.java
@@ -1,14 +1,28 @@
 package com.company.designPatterns.mediator;
 
 public class ChatMain {
+    //    Mediator pattern:
+//    You will have a system that organize the communication between students and lecturers. There will
+//    be two different students which are on-site student and remote students. The features of classes up
+//    to you. Those students will send a message to each other on the other words this app will be like
+//    messaging app. You will have Mediator which coordinates messages.s
+//    Example of box:
+//            — On-site Student A sends: Hi remote, how is going?
+//            — All On-site Students receive: Hi remote, how is going?
+//            — All Remote Students receive: Hi remote, how is going?
+//            — Remote Student A : Hi, great. I have lots of things to do, how about you?
+//            — All On-site Students receive: Hi, great. I have lots of things to do, how about you?
+//            — All Remote Students receive: Hi, great. I have lots of things to do, how about you?
+//            — On-site Student B : It similar.
+//    One should think this app like whatsapp group.
     public static void main(String[] args) {
         ChatMediator chatMediator = new ChatMediator();
         OnSiteStudent onsiteStudentA = new OnSiteStudent(chatMediator, "On-site student A");
-        OnSiteStudent onsiteStudentB = new OnSiteStudent(chatMediator, "On-site student B");
         RemoteStudent remoteStudentA = new RemoteStudent(chatMediator, "Remote student A");
-        RemoteStudent remoteStudentB = new RemoteStudent(chatMediator, "Remote student B");
 
         onsiteStudentA.sendMessage("Hi remote, how is going?");
         remoteStudentA.sendMessage("Hi, great. I have lots of things");
+        chatMediator.sendMessage(null, "Hello everyone, I'm doing great. I have lots of things to do, how about you?");
+
     }
 }
