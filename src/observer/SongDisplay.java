11d21f2cc26a1c10984816573863f4915eed99b3
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-26 20:00:43 +0300

    SongDisplay

---
+++ src/observer/SongDisplay.java
@@ -0,0 +1,20 @@
+package observer;
+
+public class SongDisplay implements Observer {
+    private boolean playing;
+    private String currentSong;
+
+    @Override
+    public void update(boolean playing) {
+        this.playing = playing;
+        if (playing) {
+            System.out.println("Şu an çalan şarkı: " + currentSong);
+        } else {
+            System.out.println("Müzik durduruldu.");
+        }
+    }
+
+    public void setCurrentSong(String song) {
+        this.currentSong = song;
+    }
+}
