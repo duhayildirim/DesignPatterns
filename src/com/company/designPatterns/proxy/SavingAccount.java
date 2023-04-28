664f3de6ed77fc2f0ebf78908b25c7e5ca8d3ffd
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-04-28 16:54:18 +0300

    balance

--- src/com/company/designPatterns/proxy/SavingAccount.java
+++ src/com/company/designPatterns/proxy/SavingAccount.java
@@ -1,17 +1,20 @@
 package com.company.designPatterns.proxy;
 
 public class SavingAccount implements BankAccount {
-
-    public SavingAccount() {
-
+    private double balance;
+    public SavingAccount(Double initialBalance) {
+        this.balance = initialBalance;
     }
 
     public boolean validate() {
+        // Validating the account
         return true;
     }
 
     @Override
     public void debit() {
-
+        // Debiting the account
+        balance -= 10;
+        System.out.println("Debited 10 TL from the account. New balance is " + balance + " TL");
     }
 }
